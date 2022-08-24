f1e3aaffa50006fdcf2f0a0e623ac5d61388e6ff
popsy <popsy2007@mail.ru>
2022-08-24 22:51:36 +0300

    add time tracking for all converters

--- run.php
+++ run.php
@@ -1,13 +1,46 @@
 <?php
 require 'vendor/autoload.php';
 
-//$template = __DIR__ . '/xml/template_25mb.xml';
-$template = __DIR__ . '/xml/template_small.xml';
+$template = __DIR__ . '/xml/template_25mb.xml';
+//$template = __DIR__ . '/xml/template_small.xml';
 
-$converter = \App\XmlReaderWriterConverter::class;
-//$converter = \App\SabreXmlFileConverter::class;
 
-(new $converter(
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$start = microtime(true);
+
+(new \App\RegexConverter(
     $template,
-    __DIR__ . '/xml/result.xml'
+    __DIR__ . '/xml/result_regex.xml'
 ))->convert();
+
+$time = microtime(true) - $start;
+dump(sprintf('RegexConverter Time: %s s.', $time));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$start = microtime(true);
+
+(new \App\XmlReaderWriterConverter(
+    $template,
+    __DIR__ . '/xml/result_XmlReaderWriter.xml'
+))->convert();
+
+$time = microtime(true) - $start;
+dump(sprintf('XmlReaderWriterConverter Time: %s s.', $time));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$start = microtime(true);
+
+(new \App\SabreXmlFileConverter(
+    $template,
+    __DIR__ . '/xml/result_Sabre.xml'
+))->convert();
+
+$time = microtime(true) - $start;
+dump(sprintf('SabreXmlFileConverter Time: %s s.', $time));
+
+
+
+
